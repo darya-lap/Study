c6dd45c0e9019af1af5eb31e9f547964945895a1
darya-lap <lapa2702>
2018-07-12 19:17:41 +0300

    fix mistake

--- src/main/java/com/dlaptsev/sbb/repository/TrainRepository.java
+++ src/main/java/com/dlaptsev/sbb/repository/TrainRepository.java
@@ -1,6 +1,5 @@
 package com.dlaptsev.sbb.repository;
 
-import com.dlaptsev.sbb.entity.Ticket;
 import com.dlaptsev.sbb.entity.Train;
 import org.springframework.data.jpa.repository.JpaRepository;
 
